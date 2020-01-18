e4f3df5c9d2f21d5f29a11109f804bd2377b0fb4
Pani Kishore Gontla <kishore@NBC02ZR128MD6T.EMIRATES.COM>
2020-01-18 22:15:37 +0400

    setup EKS and Jenkins

---
+++ provider.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region                  = "${var.region}"
+  # shared_credentials_file = "~/.aws/credentials"
+}
+
+provider "random" {
+  version = "~> 2.1"
+}
+
+provider "local" {
+  version = "~> 1.2"
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
---
+++ variables.tf
@@ -0,0 +1,61 @@
+variable "region" {
+  default = "us-east-1"
+}
+variable "environment" {
+  default = "Development"
+}
+variable "vpc_cidr" {
+  description = "VPC cidr block"
+}
+variable "instance_type" {
+
+}
+variable "instance_ami" {
+
+}
+variable "keyname" {
+  
+}
+
+variable "map_accounts" {
+  description = "Additional AWS account numbers to add to the aws-auth configmap."
+  type        = list(string)
+
+  default = [
+    "777777777777",
+  ]
+}
+
+variable "map_roles" {
+  description = "Additional IAM roles to add to the aws-auth configmap."
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = [
+    {
+      rolearn  = "arn:aws:iam::66666666666:role/role1"
+      username = "role1"
+      groups   = ["system:masters"]
+    },
+  ]
+}
+
+variable "map_users" {
+  description = "Additional IAM users to add to the aws-auth configmap."
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = [
+    {
+      userarn  = "arn:aws:iam::66666666666:user/user1"
+      username = "user1"
+      groups   = ["system:masters"]
+    },
+  ]
+}
